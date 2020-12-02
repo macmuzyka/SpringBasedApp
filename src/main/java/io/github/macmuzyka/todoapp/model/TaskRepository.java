f9e99c004facc110f60a916dd40691063c2ccb8f
macmuzyka <raweshau@gmail.com>
2020-12-02 12:59:13 +0100

    added deleteById and delete method with @RestResource annotations to prevent tasks from being removed, DELETE request will produce response status code 405 - action not allowed

--- src/main/java/io/github/macmuzyka/todoapp/model/TaskRepository.java
+++ src/main/java/io/github/macmuzyka/todoapp/model/TaskRepository.java
@@ -1,7 +1,11 @@
 package io.github.macmuzyka.todoapp.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 /**
  * Created by Raweshau
@@ -9,5 +13,16 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 
 @RepositoryRestResource
-interface TaskRepository extends JpaRepository<Task, Integer> {
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Task task);
+
+    @RestResource(path = "done", rel = "done")
+    List<Task> findByDone(@Param("state") boolean done);
+
 }
