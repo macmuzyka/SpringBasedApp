a68548533fc43583f581e0cef966cebfcd85e416
macmuzyka <raweshau@gmail.com>
2020-12-07 13:04:15 +0100

    minor code cleanup, post 4.3 Spring Mapping annotations only for all the class methods

--- src/main/java/io/github/macmuzyka/todoapp/controller/TaskController.java
+++ src/main/java/io/github/macmuzyka/todoapp/controller/TaskController.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -17,7 +16,7 @@ import java.util.List;
  * Created by Raweshau
  * on 02.12.2020
  */
-@Controller
+@RestController
 class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
@@ -49,16 +48,7 @@ class TaskController {
     }
 
 
-//    @GetMapping("/tasks/{id}")
-//    ResponseEntity<?> getTask(@PathVariable int id) {
-//        if (repository.findById(id).isPresent()) {
-//            return ResponseEntity.ok(repository.findById(id));
-//        }
-//        return ResponseEntity.notFound().build();
-//    }
-
-    // Pre 4.3 Spring mapping annotation <----------------
-    @RequestMapping(method = RequestMethod.GET, path = "/tasks/{id}")
+    @GetMapping("/tasks/{id}")
     ResponseEntity<Task> getTask(@PathVariable int id) {
         return repository.findById(id)
                 .map(task -> ResponseEntity.ok(task))
@@ -70,4 +60,34 @@ class TaskController {
         Task taskRepositorySave = repository.save(newTask);  // new Task variable to avoid code duplication in URI.create
         return ResponseEntity.created(URI.create("/" + taskRepositorySave.getId())).body(taskRepositorySave);
     }
+
+    @DeleteMapping("/tasks/{id}")
+    ResponseEntity<?> deleteById(@PathVariable int id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            logger.info("Task with ID number [" + id + "] deleted");
+            return ResponseEntity.ok().build();
+        } else {
+            logger.warn("Task with ID number [" + id + "] NOT FOUND");
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
+
+//    // Pre 4.3 Spring mapping annotation <----------------
+//    @RequestMapping(method = RequestMethod.GET, path = "/tasks/{id}")
+//    ResponseEntity<Task> getTask(@PathVariable int id) {
+//        return repository.findById(id)
+//                .map(task -> ResponseEntity.ok(task))
+//                .orElse(ResponseEntity.notFound().build());
+//    }
+
+//    @GetMapping("/tasks/{id}")
+//    ResponseEntity<?> getTask(@PathVariable int id) {
+//        if (repository.findById(id).isPresent()) {
+//            return ResponseEntity.ok(repository.findById(id));
+//        }
+//        return ResponseEntity.notFound().build();
+//    }
+//     Pre 4.3 Spring mapping annotation <----------------
+//     @RequestMapping(method = RequestMethod.GET, path = "/tasks/{id}")
