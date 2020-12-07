7fc96b9babbeb40bfb34dd41129bfa1bc4cb66aa
macmuzyka <raweshau@gmail.com>
2020-12-07 23:49:02 +0100

    Template class for nested properties with boolean allowMultipleTasks field

---
+++ src/main/java/io/github/macmuzyka/todoapp/Template.java
@@ -0,0 +1,13 @@
+package io.github.macmuzyka.todoapp;
+
+public class Template {
+    private boolean allowMultipleTasks;
+
+    public boolean isAllowMultipleTasks() {
+        return allowMultipleTasks;
+    }
+
+    public void setAllowMultipleTasks(final boolean allowMultipleTasks) {
+        this.allowMultipleTasks = allowMultipleTasks;
+    }
+}
