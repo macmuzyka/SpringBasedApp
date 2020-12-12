5db760773acc3a728493c7faeb2af5d4bd9c93d3
macmuzyka <raweshau@gmail.com>
2020-12-12 16:57:39 +0100

    @Override and @Query annotations in order to make sure that existsById methods is executed properly, by using native SQL statement

--- src/main/java/io/github/macmuzyka/todoapp/model/SqlTaskRepository.java
+++ src/main/java/io/github/macmuzyka/todoapp/model/SqlTaskRepository.java
@@ -1,6 +1,8 @@
 package io.github.macmuzyka.todoapp.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -10,4 +12,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
+    @Override
+    @Query(nativeQuery = true, value = "select count(*) > 0 from tasks where id=:id")
+    boolean existsById(@Param("id") Integer id);
 }
