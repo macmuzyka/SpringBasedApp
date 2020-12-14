74f1d4d2384b81b47fa0707854c03c7fa355e90a
macmuzyka <raweshau@gmail.com>
2020-12-14 12:43:09 +0100

    new entity class for grouping various tasks into one group

---
+++ src/main/java/io/github/macmuzyka/todoapp/model/TaskGroup.java
@@ -0,0 +1,56 @@
+package io.github.macmuzyka.todoapp.model;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Set;
+
+@Entity
+@Table(name = "task_groups")
+public class TaskGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "Task group's description MUST NOT be empty!")
+    private String description;
+    private boolean done;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "group")
+    private Set<Task> tasks;
+
+
+    TaskGroup() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    void setTasks(Set<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
