a8b58435eea3b5fbd81f335ebe1f2155e53d6d6c
macmuzyka <raweshau@gmail.com>
2020-12-14 18:49:57 +0100

    made audit class to be @MappedSuperclass for BaseTask to extend it to avoid code duplication of audit in both task and taskgroup classes

--- src/main/java/io/github/macmuzyka/todoapp/model/Audit.java
+++ src/main/java/io/github/macmuzyka/todoapp/model/Audit.java
@@ -1,11 +1,11 @@
 package io.github.macmuzyka.todoapp.model;
 
-import javax.persistence.Embeddable;
+import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
-@Embeddable
+@MappedSuperclass
 class Audit {
 
     private LocalDateTime createdOn;
