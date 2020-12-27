002c8ac5e0c440a2a4fca82240eeb02986c6bea6
macmuzyka <raweshau@gmail.com>
2020-12-27 14:51:08 +0100

    sql repository with overridden query to find not-done tasks

--- src/main/java/io/github/macmuzyka/todoapp/adapter/SqlTaskRepository.java
+++ src/main/java/io/github/macmuzyka/todoapp/adapter/SqlTaskRepository.java
@@ -1,5 +1,7 @@
-package io.github.macmuzyka.todoapp.model;
+package io.github.macmuzyka.todoapp.adapter;
 
+import io.github.macmuzyka.todoapp.model.Task;
+import io.github.macmuzyka.todoapp.model.TaskRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,6 +15,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
     @Override
-    @Query(nativeQuery = true, value = "select count(*) > 0 from tasks where id=:id")
+    @Query(nativeQuery = true, value = "select count(*) > 0 from TASKS where id=:id")
     boolean existsById(@Param("id") Integer id);
+
+    @Override
+    boolean existsByDoneIsFalseAndGroup_Id(Integer groupId);
 }
