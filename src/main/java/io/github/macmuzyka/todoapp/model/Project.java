ac20a4c3fef0a9d8e8682ba383e7d70b56c23b24
macmuzyka <raweshau@gmail.com>
2020-12-27 14:47:24 +0100

    new entity - project as a template for project steps which will be task within task group

---
+++ src/main/java/io/github/macmuzyka/todoapp/model/Project.java
@@ -0,0 +1,51 @@
+package io.github.macmuzyka.todoapp.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Set;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "Project's description field MUST NOT be empty!")
+    private String description;
+    @OneToMany(mappedBy = "project")
+    private Set<TaskGroup> groups;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "project")
+    private Set<ProjectStep> steps;
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(String description) {
+        this.description = description;
+    }
+
+    Set<TaskGroup> getGroups() {
+        return groups;
+    }
+
+    void setGroups(Set<TaskGroup> groups) {
+        this.groups = groups;
+    }
+
+    Set<ProjectStep> getSteps() {
+        return steps;
+    }
+
+    void setSteps(Set<ProjectStep> steps) {
+        this.steps = steps;
+    }
+}
