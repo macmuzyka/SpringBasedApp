d0bab5d72d2c6c3d5e0557faf662f36d8e895b4e
macmuzyka <raweshau@gmail.com>
2021-01-04 14:25:21 +0100

    exposing setter and getter for Project

--- src/main/java/io/github/macmuzyka/todoapp/model/TaskGroup.java
+++ src/main/java/io/github/macmuzyka/todoapp/model/TaskGroup.java
@@ -25,11 +25,11 @@ public class TaskGroup extends BaseTask {
         this.tasks = tasks;
     }
 
-    Project getProject() {
+    public Project getProject() {
         return project;
     }
 
-    void setProject(Project project) {
+    public void setProject(Project project) {
         this.project = project;
     }
 }
