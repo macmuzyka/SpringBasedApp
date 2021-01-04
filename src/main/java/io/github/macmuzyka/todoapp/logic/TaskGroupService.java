b1efac33472a45356b53d14d400d94bae2b7db2d
macmuzyka <raweshau@gmail.com>
2021-01-04 14:26:14 +0100

    variable name change and adding save method from TaskGroupRepository at the end of toggleGroup method

--- src/main/java/io/github/macmuzyka/todoapp/logic/TaskGroupService.java
+++ src/main/java/io/github/macmuzyka/todoapp/logic/TaskGroupService.java
@@ -13,21 +13,21 @@ import java.util.stream.Collectors;
 
 @Service
 public class TaskGroupService {
-        private TaskGroupRepository repository;
+        private TaskGroupRepository taskGroupRepository;
         private TaskRepository taskRepository;
 
     public TaskGroupService(final TaskGroupRepository repository, final TaskRepository taskRepository) {
-        this.repository = repository;
+        this.taskGroupRepository = repository;
         this.taskRepository = taskRepository;
     }
 
     public GroupReadModel createGroup(GroupWriteModel source) {
-        TaskGroup result = repository.save(source.toGroup());
+        TaskGroup result = taskGroupRepository.save(source.toGroup());
         return new GroupReadModel(result);
     }
 
     public List<GroupReadModel> readAll() {
-        return repository.findAll().stream()
+        return taskGroupRepository.findAll().stream()
                 .map(GroupReadModel::new)
                 .collect(Collectors.toList());
     }
@@ -36,8 +36,9 @@ public class TaskGroupService {
         if (taskRepository.existsByDoneIsFalseAndGroup_Id(groupId)) {
             throw new IllegalStateException("Group has undone tasks. Do all the tasks first!");
         }
-        TaskGroup result = repository.findById(groupId)
+        TaskGroup result = taskGroupRepository.findById(groupId)
                 .orElseThrow(() -> new IllegalArgumentException("TaskGroup with given id not found"));
         result.setDone(!result.isDone());
+        taskGroupRepository.save(result);
     }
 }
