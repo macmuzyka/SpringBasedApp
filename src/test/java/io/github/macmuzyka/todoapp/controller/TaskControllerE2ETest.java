175abd5554ae0d13917b38e663ad8ebd62e1d7e5
macmuzyka <raweshau@gmail.com>
2021-01-05 19:16:25 +0100

    E2E test for returning two saved tasks

---
+++ src/test/java/io/github/macmuzyka/todoapp/controller/TaskControllerE2ETest.java
@@ -0,0 +1,44 @@
+package io.github.macmuzyka.todoapp.controller;
+
+import io.github.macmuzyka.todoapp.model.Task;
+import io.github.macmuzyka.todoapp.model.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+//@ActiveProfiles("integration")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TaskControllerE2ETest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    @Qualifier("sqlTaskRepository")
+    TaskRepository repo;
+
+    @Test
+    void httpGet_returnsAllTasks() {
+        // given
+        int initial = repo.findAll().size();
+        repo.save(new Task("foo", LocalDateTime.now()));
+        repo.save(new Task("bar", LocalDateTime.now()));
+
+        // when
+        Task[] result = restTemplate.getForObject("http://localhost:" + port + "/tasks", Task[].class);
+
+        // then
+        assertThat(result).hasSize(initial + 2);
+    }
+}
