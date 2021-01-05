e1ad092d6ccf4e9ef06313cce1ef15929dd77117
macmuzyka <raweshau@gmail.com>
2021-01-05 19:14:35 +0100

    logic configuration for TaskGroupService and ProjectService to keep the mentioned classes separated from Spring

---
+++ src/main/java/io/github/macmuzyka/todoapp/logic/LogicConfiguration.java
@@ -0,0 +1,31 @@
+package io.github.macmuzyka.todoapp.logic;
+
+import io.github.macmuzyka.todoapp.TaskConfigurationProperties;
+import io.github.macmuzyka.todoapp.model.ProjectRepository;
+import io.github.macmuzyka.todoapp.model.TaskGroupRepository;
+import io.github.macmuzyka.todoapp.model.TaskRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class LogicConfiguration {
+    @Bean
+    ProjectService projectService(
+            final ProjectRepository repository,
+            final TaskGroupRepository taskGroupRepository,
+            final TaskConfigurationProperties config
+    ) {
+        return new ProjectService(repository, taskGroupRepository, config);
+    }
+
+    @Bean
+    TaskGroupService taskGroupService(
+            final TaskGroupRepository taskGroupRepository,
+            @Qualifier("sqlTaskRepository")
+            final TaskRepository taskRepository // @Profile("integration") for  TaskRepository testRepo(), but qualifier added to keep IntelliJ happy
+
+    ) {
+        return new TaskGroupService(taskGroupRepository, taskRepository);
+    }
+}
