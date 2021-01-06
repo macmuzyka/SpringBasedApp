17b1c2eb0d73e10f656a079bcb1b1552a172780d
macmuzyka <raweshau@gmail.com>
2021-01-07 00:46:59 +0100

    E2E test for returning all tasks, given task and creating new task with POST request

--- src/test/java/io/github/macmuzyka/todoapp/controller/TaskControllerE2ETest.java
+++ src/test/java/io/github/macmuzyka/todoapp/controller/TaskControllerE2ETest.java
@@ -8,14 +8,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
-//@ActiveProfiles("integration")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TaskControllerE2ETest {
     @LocalServerPort
@@ -31,7 +29,7 @@ class TaskControllerE2ETest {
     @Test
     void httpGet_returnsAllTasks() {
         // given
-        int initial = repo.findAll().size();
+        int initial = repo.findAll().size(); // java V__2 migration
         repo.save(new Task("foo", LocalDateTime.now()));
         repo.save(new Task("bar", LocalDateTime.now()));
 
@@ -40,5 +38,41 @@ class TaskControllerE2ETest {
 
         // then
         assertThat(result).hasSize(initial + 2);
+    }
+
+    @Test
+    void httpGet_returnsGivenTask() {
+        // given
+        int sizeBeforeSave = repo.findAll().size();
+        int taskId = sizeBeforeSave + 1;
+
+        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        repo.save(new Task("foobar", now));
+
+        //when
+        Task result = restTemplate.getForObject("http://localhost:" + port + "/tasks/" + taskId, Task.class);
+
+        // then
+        assertThat(result).hasFieldOrPropertyWithValue("description", "foobar");
+        assertThat(result).hasFieldOrPropertyWithValue("deadline", now);
+    }
+
+    @Test
+    void httpPost_createsNewTask() {
+        // given
+        int sizeBeforePost = repo.findAll().size();
+        int beforePostTaskIdCheck = sizeBeforePost + 1;
+
+        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        Task toPost = new Task("postTask", now);
+
+        // when
+        int postedTaskId = restTemplate.postForObject("http://localhost:" + port + "/tasks", toPost, Task.class).getId();
+        Task postedTask = restTemplate.getForObject("http://localhost:" + port + "/tasks/" + beforePostTaskIdCheck, Task.class);
+
+        // then
+        assertThat(postedTaskId).isEqualTo(beforePostTaskIdCheck);
+        assertThat(postedTask).hasFieldOrPropertyWithValue("description", "postTask");
+        assertThat(postedTask).hasFieldOrPropertyWithValue("deadline", now);
     }
 }
