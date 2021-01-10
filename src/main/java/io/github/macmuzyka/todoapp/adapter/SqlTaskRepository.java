f55e67ac848bd67e04fe4cd11a3372b4a3c0fa46
macmuzyka <raweshau@gmail.com>
2021-01-10 23:12:24 +0100

    new sql methods for finding in given group and undone tasks that exceeded deadline

--- src/main/java/io/github/macmuzyka/todoapp/adapter/SqlTaskRepository.java
+++ src/main/java/io/github/macmuzyka/todoapp/adapter/SqlTaskRepository.java
@@ -7,6 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Created by Raweshau
  * on 27.11.2020
@@ -20,4 +24,12 @@ interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer>
 
     @Override
     boolean existsByDoneIsFalseAndGroup_Id(Integer groupId);
+
+    @Override
+    Optional<List<Task>> findAllByGroup_Id(Integer groupId);
+
+    @Override
+    Optional<List<Task>> findByDoneAndDeadlineIsBeforeOrDeadlineIsNull(
+            @Param("done") boolean done,
+            @Param("deadline") LocalDateTime deadline);
 }
