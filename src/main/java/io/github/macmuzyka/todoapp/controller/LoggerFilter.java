9a7c3538fb88f2756e26753882bbd2a6ff6a7ee5
macmuzyka <raweshau@gmail.com>
2021-01-10 13:26:07 +0100

    new LoggerFilter class to display filter logs in console

---
+++ src/main/java/io/github/macmuzyka/todoapp/controller/LoggerFilter.java
@@ -0,0 +1,26 @@
+package io.github.macmuzyka.todoapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@Component
+public class LoggerFilter implements Filter {
+    public static final Logger logger = LoggerFactory.getLogger(LoggerFilter.class);
+
+    @Override
+    public void doFilter(final ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (request instanceof HttpServletRequest) {
+            var httpRequest = (HttpServletRequest) request;
+            logger.info("[doFilter] " + httpRequest.getMethod() + " " + httpRequest.getRequestURI());
+        }
+        chain.doFilter(request, response);
+    }
+}
