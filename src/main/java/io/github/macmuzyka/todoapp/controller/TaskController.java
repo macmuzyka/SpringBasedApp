753aa1749628abcb7ca1e897bca06ea97468b1fa
macmuzyka <raweshau@gmail.com>
2021-01-10 13:28:56 +0100

    added @RequestMapping annotation with "/tasks" value to avoid code duplication and showcasing old fashioned way of writing http requests (also posibility of processing requests and responds

--- src/main/java/io/github/macmuzyka/todoapp/controller/TaskController.java
+++ src/main/java/io/github/macmuzyka/todoapp/controller/TaskController.java
@@ -9,8 +9,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.io.IOException;
 import java.net.URI;
 import java.util.List;
 
@@ -19,6 +22,7 @@ import java.util.List;
  * on 02.12.2020
  */
 @RestController
+@RequestMapping("/tasks")
 class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
@@ -28,20 +32,33 @@ class TaskController {
         this.repository = repository;
     }
 
-    @GetMapping(value = "/tasks", params = {"!sort", "!page", "!size"})
+    @GetMapping(params = {"!sort", "!page", "!size"})
     ResponseEntity<List<Task>> readAllTasks() {
         logger.warn("Exposing all the tasks!");
         return ResponseEntity.ok(repository.findAll());
     }
 
-    @GetMapping("/tasks")
+    @GetMapping
     ResponseEntity<List<Task>> readAllTasks(Pageable page) {
         logger.info("Custom pageable");
         return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 
+    @GetMapping("/test")
+    void oldFashionedWay(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println(req.getParameter("foo"));
+        resp.getWriter().println("test old-fashioned way");
+    }
+
+    @GetMapping(value = "/search/done")
+    ResponseEntity<List<Task>> readDoneTasks(@RequestParam(defaultValue = "true") boolean state) {
+        return ResponseEntity.ok(
+                repository.findByDone(state)
+        );
+    }
+
     @Transactional
-    @PutMapping("/tasks/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate) {
         if (!repository.existsById(id)) {
             return ResponseEntity.notFound().build();
@@ -52,7 +69,7 @@ class TaskController {
     }
 
     @Transactional
-    @PatchMapping("/tasks/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<?> toggleTask(@PathVariable int id) {
         if (!repository.existsById(id)) {
             return ResponseEntity.notFound().build();
@@ -63,62 +80,18 @@ class TaskController {
     }
 
 
-    @GetMapping("/tasks/{id}")
+    @GetMapping("/{id}")
     ResponseEntity<Task> getTask(@PathVariable int id) {
         return repository.findById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/tasks")
+    @PostMapping
     ResponseEntity<?> addNewTask(@RequestBody @Valid Task newTask) {
         Task taskRepositorySave = repository.save(newTask);  // new Task variable to avoid code duplication in URI.create
         return ResponseEntity.created(URI.create("/" + taskRepositorySave.getId())).body(taskRepositorySave);
     }
 
-//    @DeleteMapping("/tasks/{id}")
-//    ResponseEntity<?> deleteById(@PathVariable int id) {
-//        if (repository.existsById(id)) {
-//            repository.deleteById(id);
-//            logger.info("Task with ID number [" + id + "] deleted");
-//            return ResponseEntity.ok().build();
-//        } else {
-//            logger.warn("Task with ID number [" + id + "] NOT FOUND");
-//            return ResponseEntity.notFound().build();
-//        }
-//    }
+
 }
-
-//    // Pre 4.3 Spring mapping annotation <----------------
-//    @RequestMapping(method = RequestMethod.GET, path = "/tasks/{id}")
-//    ResponseEntity<Task> getTask(@PathVariable int id) {
-//        return repository.findById(id)
-//                .map(task -> ResponseEntity.ok(task))
-//                .orElse(ResponseEntity.notFound().build());
-//    }
-
-//    @GetMapping("/tasks/{id}")
-//    ResponseEntity<?> getTask(@PathVariable int id) {
-//        if (repository.findById(id).isPresent()) {
-//            return ResponseEntity.ok(repository.findById(id));
-//        }
-//        return ResponseEntity.notFound().build();
-//    }
-//     Pre 4.3 Spring mapping annotation <----------------
-//     @RequestMapping(method = RequestMethod.GET, path = "/tasks/{id}")
-
-//    METHOD WITHOUT @Transactional annotation must have manual repository.save(task)
-//    code in order for PUT request to work, tasks will be updated even if the exception is thrown, not like
-//    in @Transactional annotation where all the steps must succeed in order for task to be saved
-//    @PutMapping("/tasks/{id}")
-//    ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate) {
-//        if (!repository.existsById(id)) {
-//            return ResponseEntity.notFound().build();
-//        }
-//        repository.findById(id)
-//                .ifPresent(task -> {
-//                    task.updateFrom(toUpdate);
-//                    repository.save(task);
-//                });
-//        return ResponseEntity.noContent().build();
-//    }
