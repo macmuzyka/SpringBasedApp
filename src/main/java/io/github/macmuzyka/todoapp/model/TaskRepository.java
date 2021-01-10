c7ea9714332e9a33d0639ad87e9f2a8c7a5602d1
macmuzyka <raweshau@gmail.com>
2021-01-10 23:14:53 +0100

    two new methods corresponding to it's sql methods

--- src/main/java/io/github/macmuzyka/todoapp/model/TaskRepository.java
+++ src/main/java/io/github/macmuzyka/todoapp/model/TaskRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,5 +21,11 @@ public interface TaskRepository {
 
     List<Task> findByDone(@Param("state") boolean done);
 
+    Optional<List<Task>> findAllByGroup_Id(Integer groupId);
+
     Task save(Task entity);
+
+    Optional<List<Task>> findByDoneAndDeadlineIsBeforeOrDeadlineIsNull(
+            @Param("done") boolean done,
+            @Param("deadline") LocalDateTime deadline);
 }
