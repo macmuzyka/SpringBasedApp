6aab4da3bbfbdb7840e72d983150f1387c45502c
macmuzyka <raweshau@gmail.com>
2021-01-14 19:45:46 +0100

    setting project to newly created TaskGroup from toGroup method

--- src/main/java/io/github/macmuzyka/todoapp/model/projection/GroupWriteModel.java
+++ src/main/java/io/github/macmuzyka/todoapp/model/projection/GroupWriteModel.java
@@ -1,5 +1,6 @@
 package io.github.macmuzyka.todoapp.model.projection;
 
+import io.github.macmuzyka.todoapp.model.Project;
 import io.github.macmuzyka.todoapp.model.TaskGroup;
 
 import java.util.Set;
@@ -25,7 +26,7 @@ public class GroupWriteModel {
         this.tasks = tasks;
     }
 
-    public TaskGroup toGroup() {
+    public TaskGroup toGroup(Project project) {
         var result = new TaskGroup();
         result.setDescription(description);
         result.setTasks(
@@ -33,6 +34,7 @@ public class GroupWriteModel {
                         .map(source -> source.toTask(result))
                         .collect(Collectors.toSet())
         );
+        result.setProject(project);
         return result;
     }
 }
