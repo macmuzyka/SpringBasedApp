925d21b06a0701658668e74d99987fee3e507d68
macmuzyka <raweshau@gmail.com>
2021-01-14 19:49:10 +0100

    project controller with following methods: showProject(mvc), addProject, addProjectStep, createProject and getProjects

---
+++ src/main/java/io/github/macmuzyka/todoapp/controller/ProjectController.java
@@ -0,0 +1,76 @@
+package io.github.macmuzyka.todoapp.controller;
+
+import io.github.macmuzyka.todoapp.logic.ProjectService;
+import io.github.macmuzyka.todoapp.model.Project;
+import io.github.macmuzyka.todoapp.model.ProjectStep;
+import io.github.macmuzyka.todoapp.model.projection.ProjectWriteModel;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Controller
+@RequestMapping("/projects")
+class ProjectController {
+    private final ProjectService service;
+
+    ProjectController(final ProjectService service) {
+        this.service = service;
+    }
+
+
+    @GetMapping
+    String showProjects(Model model) {
+        model.addAttribute("project", new ProjectWriteModel());
+        return "projects";
+    }
+
+    @PostMapping
+    String addProject(
+            @ModelAttribute("project") @Valid ProjectWriteModel current,
+            BindingResult bindingResult,
+            Model model
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "projects";
+        }
+        service.save(current);
+        model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("projects", getProjects());
+        model.addAttribute("message", "Project added!");
+        return "projects";
+    }
+
+    @PostMapping(params = "addStep")
+    String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
+        current.getSteps().add(new ProjectStep());
+        return "projects";
+    }
+
+    @PostMapping("/{id}")
+    String createGroup(
+            @ModelAttribute("project") ProjectWriteModel current,
+            Model model,
+            @PathVariable int id,
+            @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime deadline
+    ) {
+        try {
+            service.createGroup(id, deadline);
+            model.addAttribute("message", "Group added!");
+        } catch (IllegalStateException | IllegalArgumentException e) {
+            model.addAttribute("message", "Error occurred when creating a group!");
+        }
+        return "projects";
+
+    }
+
+    @ModelAttribute("projects")
+    List<Project> getProjects() {
+        return service.readAll();
+    }
+}
