e76e344d74595b52d830dd82e1ac0fa43b2c495c
macmuzyka <raweshau@gmail.com>
2021-01-14 19:44:46 +0100

    test for checking if deadline based tasks update deadline to null properly

---
+++ src/test/java/io/github/macmuzyka/todoapp/model/projection/GroupReadModelTest.java
@@ -0,0 +1,27 @@
+package io.github.macmuzyka.todoapp.model.projection;
+
+import io.github.macmuzyka.todoapp.model.Task;
+import io.github.macmuzyka.todoapp.model.TaskGroup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class GroupReadModelTest {
+    @Test
+    @DisplayName("Should create null deadline field for Group Task when no Task deadlines")
+    void constructor_noDeadlines_createsNullDeadline() {
+        // given
+        var source = new TaskGroup();
+        source.setDescription("foo");
+        source.setTasks(Set.of(new Task("bar", null)));
+
+        // when
+        var result = new GroupReadModel(source);
+
+        // then
+        assertThat(result).hasFieldOrPropertyWithValue("deadline", null);
+    }
+}
