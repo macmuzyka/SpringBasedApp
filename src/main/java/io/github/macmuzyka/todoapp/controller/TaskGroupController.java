c27031fc0f3c3df1b9675b0432c1aa732c4f55da
macmuzyka <raweshau@gmail.com>
2021-01-17 20:27:52 +0100

    adjusted controller to service both thymeleaf templates and @RestController json @ResponseBody http requests

--- src/main/java/io/github/macmuzyka/todoapp/controller/TaskGroupController.java
+++ src/main/java/io/github/macmuzyka/todoapp/controller/TaskGroupController.java
@@ -3,10 +3,15 @@ package io.github.macmuzyka.todoapp.controller;
 import io.github.macmuzyka.todoapp.logic.TaskGroupService;
 import io.github.macmuzyka.todoapp.model.TaskRepository;
 import io.github.macmuzyka.todoapp.model.projection.GroupReadModel;
+import io.github.macmuzyka.todoapp.model.projection.GroupTaskWriteModel;
 import io.github.macmuzyka.todoapp.model.projection.GroupWriteModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
@@ -14,8 +19,8 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
-@RestController
-@RequestMapping("/tasks_groups")
+@Controller
+@RequestMapping("/groups")
 public class TaskGroupController {
 
     private Logger logger = LoggerFactory.getLogger(TaskGroupController.class);
@@ -29,13 +34,15 @@ public class TaskGroupController {
 
     }
 
-    @GetMapping
+    @ResponseBody
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<List<GroupReadModel>> readAllGroups() {
         logger.info("Exposing all the Task Groups!");
         return ResponseEntity.ok().body(taskGroupService.readAll());
     }
 
-    @GetMapping("/{id}/tasks")
+    @ResponseBody
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<?> getGivenGroupTasks(@PathVariable int id) {
         logger.info("Exposing given task!");
         return taskRepository.findAllByGroup_Id(id)
@@ -43,15 +50,17 @@ public class TaskGroupController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @ResponseBody
     @Transactional
-    @PatchMapping("/{id}")
+    @PatchMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> toggleGroup(@PathVariable int id) {
-        logger.info("Toggling given task!");
+        logger.info("Toggling given group!");
         taskGroupService.toggleGroup(id);
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping
+    @ResponseBody
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<GroupReadModel> addNewGroup(@RequestBody @Valid GroupWriteModel toSave) {
         logger.info("Posting new Task Group!");
         GroupReadModel newGroup = taskGroupService.createGroup(toSave);
@@ -66,5 +75,37 @@ public class TaskGroupController {
     @ExceptionHandler(IllegalStateException.class)
     ResponseEntity<String> handleIllegalState(IllegalStateException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
+    String showGroups(Model model) {
+        model.addAttribute("group", new GroupWriteModel());
+        return "groups";
+    }
+
+    @PostMapping(params = "addTask", produces = MediaType.APPLICATION_XHTML_XML_VALUE)
+    String addTask(@ModelAttribute("group") GroupWriteModel newGroup) {
+        newGroup.getTasks().add(new GroupTaskWriteModel());
+        return "groups";
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_XHTML_XML_VALUE, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    String addGroup(@ModelAttribute("group") @Valid GroupWriteModel newGroup,
+                    BindingResult bindingResult,
+                    Model model
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "groups";
+        }
+        taskGroupService.createGroup(newGroup);
+        model.addAttribute("group", new GroupWriteModel());
+        model.addAttribute("groups", getGroups());
+        model.addAttribute("message", "Group added!");
+        return "groups";
+    }
+
+    @ModelAttribute("groups")
+    List<GroupReadModel> getGroups() {
+        return taskGroupService.readAll();
     }
 }
