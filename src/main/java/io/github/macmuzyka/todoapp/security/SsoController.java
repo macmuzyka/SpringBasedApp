8561c24020ffbfa48c85228b43280592636e028e
macmuzyka <raweshau@gmail.com>
2021-01-31 00:51:05 +0100

    single sign on logout

---
+++ src/main/java/io/github/macmuzyka/todoapp/security/SsoController.java
@@ -0,0 +1,16 @@
+package io.github.macmuzyka.todoapp.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class SsoController {
+    @GetMapping("/logout")
+    String logout(HttpServletRequest request) throws ServletException {
+        request.logout();
+        return "index";
+    }
+}
